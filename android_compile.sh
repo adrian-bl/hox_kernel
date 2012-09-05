f8ee94a5070455c6d1cfc76dbcc55f625b524685
Adrian Ulrich <adrian@blinkenlights.ch>
2012-09-05 18:25:23 +0200

    clean

--- android_compile.sh
+++ android_compile.sh
@@ -1,4 +1,7 @@
 TCHAIN=/BUILD/CM10/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+
+
+make ARCH=arm clean
 make ARCH=arm CROSS_COMPILE=$TCHAIN endeavoru_android_defconfig 
 patch -p0 --verbose < patch_no_wext.diff
 make ARCH=arm CROSS_COMPILE=$TCHAIN -j8 
