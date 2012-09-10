e76bff3b3af2f20c588d46b190f7d42e242c4e5b
Adrian Ulrich <adrian@blinkenlights.ch>
2012-09-10 16:47:39 +0200

    add clean to buildscript

--- android_compile.sh
+++ android_compile.sh
@@ -1,3 +1,4 @@
 TCHAIN=/BUILD/CM10/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+make ARCH=arm clean
 make ARCH=arm CROSS_COMPILE=$TCHAIN endeavoru_android_defconfig 
 make ARCH=arm CROSS_COMPILE=$TCHAIN -j8 
